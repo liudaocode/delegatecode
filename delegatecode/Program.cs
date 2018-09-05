1b1e87f4399dce9dd60a06ae6ec61a38bccd3e4f
liudaocode <18260346217@163.com>
2018-09-05 17:21:42 +0800

    事件其实没什么不好理解的，声明一个事件不过类似于声明一个进行了封装的委托类型的变量而已。

--- delegatecode/Program.cs
+++ delegatecode/Program.cs
@@ -7,6 +7,21 @@ using System.Text;
 namespace delegatecode
 {
     public delegate void GreetingDelegate(string name);
+
+    public class GreetingManager
+    {
+        //在类的内部，不管你声明它是public还是protected，它总是private的。在类的外部，注册“+=”和注销“-=”的访问限定符与你在声明事件时使用的访问符相同。
+        public event GreetingDelegate delegate1;
+
+        public void GreetPeople(string name)
+        {
+            if (delegate1 != null)
+            {     //如果有方法注册委托变量
+                delegate1(name);      //通过委托调用方法
+            }
+        }
+    }
+
     class Program
     {
         private static void GreetPeople(string name, GreetingDelegate MakeGreeting)
@@ -26,12 +41,19 @@ namespace delegatecode
         }
         static void Main(string[] args)
         {
-            GreetingDelegate delegate1;
-            delegate1 = EnglishGreeting;
-            delegate1 += ChineseGreeting;
-            GreetPeople("liudao", delegate1);
-            delegate1-=ChineseGreeting;
-            GreetPeople("六道", delegate1);
+            GreetingManager gm = new GreetingManager();
+            gm.delegate1 += EnglishGreeting;
+            gm.delegate1 += ChineseGreeting;
+
+            gm.GreetPeople("liudao");
+
+
+            //GreetingDelegate delegate1;
+            //delegate1 = EnglishGreeting;
+            //delegate1 += ChineseGreeting;
+            //GreetPeople("liudao", delegate1);
+            //delegate1-=ChineseGreeting;
+            //GreetPeople("六道", delegate1);
             //Console.ReadKey();
         }
     }
