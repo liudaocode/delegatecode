c62f157ea7bfda1c34d4792ce7ea86a730133cb4
liudaocode <18260346217@163.com>
2018-09-05 14:55:46 +0800

    添加项目文件。

---
+++ delegatecode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace delegatecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
