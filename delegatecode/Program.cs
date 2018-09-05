e9f6a5080395b82ce2b5743b9f8d4b213e3764f3
liudaocode <18260346217@163.com>
2018-09-05 16:16:02 +0800

    委托是一个类，它定义了方法的类型，使得可以将方法当作另一个方法的参数来进行传递，这种将方法动态地赋给参数的做法，可以避免在程序中大量使用If-Else(Switch)语句，同时使得程序具有更好的可扩展性。

--- delegatecode/Program.cs
+++ delegatecode/Program.cs
@@ -5,10 +5,27 @@ using System.Text;
 
 namespace delegatecode
 {
+    public delegate void GreetingDelegate(string name);
     class Program
     {
+        private static void GreetPeople(string name, GreetingDelegate MakeGreeting)
+        {
+            // 做某些额外的事情，比如初始化之类，此处略
+            MakeGreeting(name);
+        }
+        public static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Morning, " + name);
+        }
+        private static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("早上好, " + name);
+        }
         static void Main(string[] args)
         {
+            GreetPeople("liudao", EnglishGreeting);
+            GreetPeople("六道", ChineseGreeting);
+            Console.ReadKey();
         }
     }
 }
